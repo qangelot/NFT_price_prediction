a1805fe248c5bef3342951f4c50026c3b4d8085d
Quentin ANGELOT <57401552+qangelot@users.noreply.github.com>
2023-03-02 13:15:13 +0100

    Update testapi.py

--- testapi.py
+++ testapi.py
@@ -3,20 +3,21 @@ import json
 import pandas as pd
 
 # Define the endpoint URL
-url = "http://localhost:5000/predict"
+url = "http://localhost:8080/predict"
 
 # Generate test data 
-data = pd.read_csv('data/filtered_data.csv' , index_col=None, header=0, lineterminator='\n')
-data = data.drop(['price_label', 'avg_selling_price', 'sale_label'],axis=1)
+data = pd.read_csv('data/new_NFTs.csv' , index_col=None, header=0, lineterminator='\n')
+# data = data.drop(['price_label', 'avg_selling_price', 'average_price'],axis=1)
+data = data.tail(50)
 
-# Get the first row of data and define the JSON payload
-data_json = data.iloc[0].to_dict()
+# Convert the dataframe to a list of dictionaries
+data_json = data.to_dict('records')
 
-# Send the request with the JSON payload
-response = requests.post(url, json=data_json)
+# Send a request for each data point in the list
+for i, item in enumerate(data_json):
+    
+    # Send the request with the JSON payload
+    response = requests.post(url, json=item)
 
-# Get the prediction from the response
-prediction = response.json()["prediction"]
-
-# Print the prediction
-print(prediction)
+    # Print the prediction
+    print(response.text)
